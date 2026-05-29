dbc11ec3bf27b4ee8e9a65db9a3e808c359fdcb9
thatipallyvinodkumar167 <thatipallyvinodkumar167@gmail.com>
2026-05-29 15:34:46 +0530

    update

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -115,7 +115,7 @@ const Orders = () => {
   const fetchOrders = async () => {
     try {
       setLoading(true);
-      const response = await fetch(`${API_BASE_URL}/orders`);
+      const response = await fetch(`${API_BASE_URL}/`);
       const resData = await response.json();
       if (resData.success) {
         setOrdersData(resData.data);
